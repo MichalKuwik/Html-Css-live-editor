63c51c06843c99edc324b110b66afd7dc884aff1
Michał Kuwik <Michal7074@poczta.fm>
2019-05-15 15:25:27 +0200

    add csseditor using ace.js library

--- js/index.js
+++ js/index.js
@@ -3,16 +3,30 @@
 //function setEditor
 const setEditor = () => {
 
+  ace.require("ace/ext/language_tools"); 
   //html editor
   window.editor = ace.edit('htmleditor', {
-    theme: "ace/theme/dracula",
-    mode: "html",
+    theme: "ace/theme/tomorrow_night_eighties",
+    mode: "ace/mode/html",
     maxLines: 30,
     wrap: true,
     autoScrollEditorIntoView: true,
     tabSize:1,
     highlightActiveLine: true,
   })
+
+  //css editor
+  window.editor = ace.edit('csseditor', {
+    theme: "ace/theme/tomorrow_night_eighties",
+    mode: "ace/mode/css",
+    maxLines: 30,
+    wrap: true,
+    autoScrollEditorIntoView: true,
+    tabSize:1,
+    highlightActiveLine: true,
+  })
+
+  
 
 }
 
